34da813e3a5b0888ae0357a6a931064160fa91ae
Admin <sorokivskyi777@gmail.com>
2023-01-25 18:00:12 +0200

    lesson3_training

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import {CarForm,Cars} from "./components";
 import {useEffect, useState} from "react";
 import {carService} from "./services";
+import axios from "axios";
 
 const App = () => {
 
     const [cars, setCars] = useState([]);
     const [updateCar,setUpdateCar] = useState(null);
-
 
     useEffect(() => {
         carService.getAll().then(({data}) => setCars(data));
--- src/components/CarForm/CarForm.js
+++ src/components/CarForm/CarForm.js
@@ -1,4 +1,4 @@
-import {useForm} from "react-hook-form";
+import {set, useForm} from "react-hook-form";
 import {joiResolver} from "@hookform/resolvers/joi";
 
 import {carValidator} from "../../validators/carValidator";
@@ -12,6 +12,9 @@ const CarForm = ({setCars,updateCar}) => {
 
     useEffect(() => {
         if (updateCar) {
+            setValue('brand', updateCar.brand);
+            setValue('price', updateCar.price);
+            setValue('year', updateCar.year);
 
         }
     },[updateCar]);
@@ -55,7 +58,7 @@ const CarForm = ({setCars,updateCar}) => {
             <input type="text" placeholder={'year'} {...register('year')}/>
             {errors.year&& <span>{errors.year.message}</span>}
 
-            <button disabled={!isValid}>SAVE</button>
+            <button disabled={!isValid}>{updateCar?'UPDATE':'SAVE'}</button>
 
         </form>
     );
