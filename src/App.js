b245f07ef09d14d22207fc0c2f82df87028eb780
Admin <sorokivskyi777@gmail.com>
2023-01-30 10:22:46 +0200

    lesson4_hw

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import {Route, Routes} from "react-router-dom";
+import {AlbumsPage} from "./pages";
+import {MainLayout} from "./layouts/MainLayout/MainLayout";
+import {TodosPage} from "./pages/TodosPage/TodosPage";
+import {HomePage} from "./pages/HomePage/HomePage";
+import {CommentsPage} from "./pages/CommentsPage/CommentsPage";
+
 const App = () => {
     return (
-        <div>
-            Appp
-        </div>
+        <Routes>
+            <Route path={'/'} element={<MainLayout/>}>
+
+                <Route index element={<HomePage/>}/>
+                <Route path={'todos'} element={<TodosPage/>}/>
+                <Route path={'albums'} element={<AlbumsPage/>}/>
+                <Route path={'comments'} element={<CommentsPage/>}/>
+
+            </Route>
+
+
+
+        </Routes>
     );
 };
 
