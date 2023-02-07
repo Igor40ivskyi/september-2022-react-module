c68e8d3b89f58b4c4967c86b91c3c26ede8a0b84
Admin <sorokivskyi777@gmail.com>
2023-02-07 14:42:11 +0200

    lesson5_useReducer_part1_learning

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import {Cats} from "./components/Cats/Cats";
+import {Dogs} from "./components/Dogs/Dogs";
+
+import css from './App.module.css';
+import {useRef} from "react";
+
 const App = () => {
+
+    const catInp = useRef();
+    const dogInp = useRef();
+
+    const createCat = () => {
+
+    };
+
+    const clreateDog = () => {
+
+    };
+
     return (
-        <div>
-            Appp
+        <div className={css.App}>
+            <div>
+                <input type="text" ref={catInp}/>
+                <button onClick={()=>createCat}>CREATE CAT</button>
+                <Cats/>
+            </div>
+            <div>
+                <input type="text" ref={dogInp}/>
+                <button onClick={clreateDog}>CREATE DOG</button>
+                <Dogs/>
+            </div>
         </div>
     );
 };
